40879cd37520a204f2f3e9839d1d0f2c41ea65ef
Dhasayuga <95868792+Dhasayuga@users.noreply.github.com>
2022-07-15 21:24:31 +0530

    update close
    update close

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(document).click(
+        function (event) {
+            var target = $(event.target);
+            var _mobileMenuOpen = $(".navbar-collapse").hasClass("show");
+            if (_mobileMenuOpen === true && !target.hasClass("navbar-toggler")) {
+                $("button.navbar-toggler").click();
+            }
+        }
+    );
+});
